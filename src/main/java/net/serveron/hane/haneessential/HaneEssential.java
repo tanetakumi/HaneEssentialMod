a6da3187e3c297302cc411074b87053b6e2ec602
BuildTools <unconfigured@null.spigotmc.org>
2021-03-27 08:53:28 +0900

    change main file

--- src/main/java/net/serveron/hane/haneessential/HaneEssential.java
+++ src/main/java/net/serveron/hane/haneessential/HaneEssential.java
@@ -1,29 +1,19 @@
 package net.serveron.hane.haneessential;
 
-import com.mojang.blaze3d.matrix.MatrixStack;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.entity.player.ClientPlayerEntity;
-import net.minecraft.resources.FolderPack;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.loading.moddiscovery.ModInfo;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 @OnlyIn(Dist.CLIENT)
 @Mod("hane")
 public class HaneEssential {
     private final Minecraft mc = Minecraft.getInstance();
-    //private final ClientPlayerEntity player;
-
 
     public HaneEssential() {
         MinecraftForge.EVENT_BUS.register(this);
@@ -31,7 +21,6 @@ public class HaneEssential {
 
     @SubscribeEvent
     public void onClientChatReceived(ClientChatReceivedEvent e) {
-        System.out.println(getModList()+getResourcesList());
         if(e.getMessage().getString().trim().equalsIgnoreCase("Authentication")){
             if(mc.player!=null){
                 mc.player.sendChatMessage("/auth "+getModList()+getResourcesList());
