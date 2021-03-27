f6762b9f5a0aa9d030341ba5b2ccf1937e7c465d
BuildTools <unconfigured@null.spigotmc.org>
2021-03-27 10:06:47 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# HaneEssentialMod BackVer
+
+Authentication Mod For Connect a player to Hane Server.
+
+This mod is "HaneEssentialMod" for old minecraft
+
+[HaneEssential](https://github.com/tanetakumi/HaneEssentialMod)
+
+### Features
+
+* get resources files (For prohibition Xray etc..) 
+* get mods files (For prohibition Cheat) 
+* ver 1.12 ~ 
+
+### Notes
+
+   This mod cannot achieve its purpose by itself. Must be used in combination with a plugin.
---
+++ src/main/java/net/serveron/hane/haneessentialbackver/HaneEssentialBackVer.java
@@ -0,0 +1,84 @@
+package net.serveron.hane.haneessentialbackver;
+
+import net.minecraft.block.Block;
+import net.minecraft.client.Minecraft;
+import net.minecraft.item.Item;
+import net.minecraftforge.client.event.ClientChatReceivedEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import scala.collection.parallel.ParIterableLike;
+
+import java.io.File;
+
+@Mod(
+        modid = HaneEssentialBackVer.MOD_ID,
+        name = HaneEssentialBackVer.MOD_NAME,
+        version = HaneEssentialBackVer.VERSION
+)
+public class HaneEssentialBackVer {
+
+    public static final String MOD_ID = "hane";
+    public static final String MOD_NAME = "HaneEssentialBackVer";
+    public static final String VERSION = "1.0-SNAPSHOT";
+
+    private Minecraft mc = Minecraft.getMinecraft();
+
+    @Mod.Instance(MOD_ID)
+    public static HaneEssentialBackVer INSTANCE;
+
+    @Mod.EventHandler
+    public void preinit(FMLPreInitializationEvent event) {
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    public void onClientChatReceived(ClientChatReceivedEvent e) {
+        if(e.getMessage().toString().trim().equalsIgnoreCase("Authentication")){
+            if(mc.player!=null){
+                mc.player.sendChatMessage("/auth "+getModList()+getResourcesList());
+            }
+        }
+    }
+
+    private String getModList(){
+        StringBuilder result = new StringBuilder();
+        String[] resourcesList = new File(mc.gameDir.getAbsolutePath(),"mods").list();
+        if(resourcesList!=null){
+            for(String item : resourcesList){
+                String text = item.replaceAll("[0-9]", "")
+                        .replace(" ","")
+                        .replace(".","")
+                        .replace("-","")
+                        .replace("zip","");
+                result.append(text).append(":");
+            }
+        } else {
+            result.append("modsNotExists").append(":");
+        }
+        return result.toString();
+    }
+
+    private String getResourcesList(){
+        StringBuilder result = new StringBuilder();
+        String[] resourcesList = new File(mc.gameDir.getAbsolutePath(),"resourcepacks").list();
+        if(resourcesList!=null){
+            for(String item : resourcesList){
+                String text = item.replaceAll("[0-9]", "")
+                        .replace(" ","")
+                        .replace(".","")
+                        .replace("-","")
+                        .replace("zip","");
+                result.append(text).append(":");
+            }
+        } else {
+            result.append("modsNotExists").append(":");
+        }
+        return result.toString();
+    }
+}
