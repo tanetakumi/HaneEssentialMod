82be8d4d73b7b75811612ad9cf34a5b77ef2956e
BuildTools <unconfigured@null.spigotmc.org>
2021-03-27 17:34:54 +0900

    detele unnecessary import

--- src/main/java/net/serveron/hane/haneessentialbackver/HaneEssentialBackVer.java
+++ src/main/java/net/serveron/hane/haneessentialbackver/HaneEssentialBackVer.java
@@ -1,18 +1,11 @@
 package net.serveron.hane.haneessentialbackver;
 
-import net.minecraft.block.Block;
 import net.minecraft.client.Minecraft;
-import net.minecraft.item.Item;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.registry.GameRegistry;
-import scala.collection.parallel.ParIterableLike;
 
 import java.io.File;
 
@@ -27,7 +20,7 @@ public class HaneEssentialBackVer {
     public static final String MOD_NAME = "HaneEssentialBackVer";
     public static final String VERSION = "1.0-SNAPSHOT";
 
-    private Minecraft mc = Minecraft.getMinecraft();
+    private final Minecraft mc = Minecraft.getMinecraft();
 
     @Mod.Instance(MOD_ID)
     public static HaneEssentialBackVer INSTANCE;
